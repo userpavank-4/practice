32cee0ac0138a76805cc06b8f8743294cf4e819c
userpavank-4 <userpavank@gmail.com>
2025-03-06 21:48:29 +0530

    Add files via upload

---
+++ gitHUB/reactjs-practice/practice-2/index.js
@@ -0,0 +1,14 @@
+const element = (
+  // Write your code here.
+  <div className="container">
+    <h1>Congratulations</h1>
+    <div className="container-2">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h2>Kiran V</h2>
+      <p>Vishnu Institute of computer education and Technology, Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
